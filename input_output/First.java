ec35c8de2627c0f5d2f48a9186f4436077171380
Vickypol09 <vickypol1331@gmail.com>
2025-08-17 12:39:24 +0530

    h

---
+++ input_output/First.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class First {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine(); //nextint() ,nextfloat()
+        System.out.println(name);
+    }
+}
